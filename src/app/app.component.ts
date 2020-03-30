06c39c8362687d3933e533b92f55c3a32810873d
Mukul Vashistha <mukulvashistha@Mukuls-MacBook-Air.local>
2020-03-30 09:00:03 +0300

    token and channel name added

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,6 +1,5 @@
-
 import { Component } from '@angular/core';
-import { AngularAgoraRtcService, Stream } from 'angular-agora-rtc'; // Add
+import { AngularAgoraRtcService, Stream } from 'angular-agora-rtc';
 
 @Component({
   selector: 'app-root',
@@ -9,20 +8,24 @@ import { AngularAgoraRtcService, Stream } from 'angular-agora-rtc'; // Add
 })
 export class AppComponent {
   title = 'AgoraDemo';
-  localStream: Stream 
+  localStream: Stream
+  remoteCalls: any = [];
+  token = '0063a28def90cd047489223531ddbdce690IADkrZ70IfPGc/9GBDHSkecxbKP0j4gC8WjH7DCMe3ELXBz5yXMAAAAAEABDPDrSDdyCXgEAAQAM3IJe';
 
-  constructor(private agoraService: AngularAgoraRtcService) {
+  constructor(
+    private agoraService: AngularAgoraRtcService
+  ) {
     this.agoraService.createClient();
   }
 
   startCall() {
-    this.agoraService.client.join(null, '1000', null, (uid) => {
+    this.agoraService.client.join(this.token, 'sports', null, (uid) => {
       this.localStream = this.agoraService.createStream(uid, true, null, null, true, false);
       this.localStream.setVideoProfile('720p_3');
       this.subscribeToStreams();
     });
   }
-  
+
   private subscribeToStreams() {
     this.localStream.on("accessAllowed", () => {
       console.log("accessAllowed");
@@ -44,5 +47,46 @@ export class AppComponent {
     }, function (err) {
       console.log("getUserMedia failed", err);
     });
+
+    this.agoraService.client.on('error', (err) => {
+      console.log("Got error msg:", err.reason);
+      if (err.reason === 'DYNAMIC_KEY_TIMEOUT') {
+        this.agoraService.client.renewChannelKey("", () => {
+          console.log("Renew channel key successfully");
+        }, (err) => {
+          console.log("Renew channel key failed: ", err);
+        });
+      }
+    });
+
+    this.agoraService.client.on('stream-added', (evt) => {
+      const stream = evt.stream;
+      this.agoraService.client.subscribe(stream, (err) => {
+        console.log("Subscribe stream failed", err);
+      });
+    });
+
+    this.agoraService.client.on('stream-subscribed', (evt) => {
+      const stream = evt.stream;
+      if (!this.remoteCalls.includes(`agora_remote${stream.getId()}`)) this.remoteCalls.push(`agora_remote${stream.getId()}`);
+      setTimeout(() => stream.play(`agora_remote${stream.getId()}`), 2000);
+    });
+
+    this.agoraService.client.on('stream-removed', (evt) => {
+      const stream = evt.stream;
+      stream.stop();
+      this.remoteCalls = this.remoteCalls.filter(call => call !== `#agora_remote${stream.getId()}`);
+      console.log(`Remote stream is removed ${stream.getId()}`);
+    });
+
+    this.agoraService.client.on('peer-leave', (evt) => {
+      const stream = evt.stream;
+      if (stream) {
+        stream.stop();
+        this.remoteCalls = this.remoteCalls.filter(call => call === `#agora_remote${stream.getId()}`);
+        console.log(`${evt.uid} left from this channel`);
+      }
+    }); 
   }
+  
 }
